1cc5a06fd3a9e3def0f04cfe6ec9bd88ff4d8c27
shrimantm <shrimantmarathe2005@gmail.com>
2026-03-26 20:36:13 +0530

    Update registration deadline to 23:59 on March 26, 2026 in GenesisPage and Home components

--- src/pages/GenesisPage.jsx
+++ src/pages/GenesisPage.jsx
@@ -112,7 +112,7 @@ const majorEvents = [
   },
 ];
 
-const REGISTRATION_DEADLINE = '2026-03-26T21:00:00+05:30';
+const REGISTRATION_DEADLINE = '2026-03-26T23:59:00+05:30';
 const REGISTRATION_ALERT = 'Registrations Closing Soon';
 const SEATS_ALERT = 'Few Seats Left';
 
